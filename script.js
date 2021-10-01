ca3abe01b130a30e1129bb405c952cc0ac5d4d78
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-10-01 18:59:09 -0500

    Not working

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var userTaskEl = document.getElementById("userTask");
+//var userTaskEl = document.getElementById("userTask");
 // var timeSlotEl = document.getElementById("timeSlot");
 // var saveBtnEl = document.querySelector("#saveBtn");
 
 
-// var userTaskEl = $('userTask');
+ var userTaskEl = $('userTask');
 // var timeSlotEl = $('timeSlot');
 // var saveBtnEl = $('saveBtn');
 
 
 $(document).ready(function(){
   $("#saveBtn").click(function(){
-    $("#userTask").val(userTaskEl.textContent);
-    console.log(userTaskEl.textContent);
+    const task = $("#userTask").val();
+    console.log(task);
+    localStorage.setItem("task", task);
+   // localStorage.getItem(task) = userTaskEl.text();
+
   })
 })
 
